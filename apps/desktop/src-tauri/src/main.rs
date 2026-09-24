be74f1d1a61720fa0c8fa1fe50edbced0373df1a
Mohamed Sabry <mohamedsabrydev3@gmail.com>
2026-09-25 01:13:01 +0300

    Split Tauri library entrypoint from Windows main

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,12 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    branch1_lib::run()
-}
-
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running Branch1");
+    branch1_lib::run();
 }
